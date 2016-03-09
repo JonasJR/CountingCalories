2022f0335e42627b2fc98a46aca585f62daa74e5
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2016-03-09 13:09:25 +0100

    Refactor search functionallity

--- js/index.js
+++ js/index.js
@@ -1,48 +1,52 @@
-var sugg;
-var callurl;
-$("#foodinput").on("input", function(){
+var callurl = "http://matapi.se/foodstuff";
+
+$("#food-input").on("input", function() {
+
+  var foodInput = $("#food-input");
+  var searchList = $("#search-list");
+
   console.log("hejsan!");
-  if($("#foodinput").val().length > 2){
-    sugg = $("#foodinput").val();
-    console.log(sugg);
-    callurl = "http://matapi.se/foodstuff";
+  if (foodInput.val().length > 2) {
+
+    var searchTerm = foodInput.val();
+
     $.ajax({
       url: callurl,
-      dataType : "JSON",
+      dataType: "JSON",
       data: {
-        query: sugg
+        query: searchTerm
       }
-    }).done(function(data){
+    }).done(function(data) {
       console.log(data);
-      var index;
-      $("#ulfood").empty();
-      for(index = 0; index < 10 ; index ++){
-        if(data[index]== undefined){
+
+      searchList.empty();
+      for (var index = 0; index < 10; index++) {
+        if (data[index] == undefined) {
           break;
         }
-        $("#ulfood").append("<li class='list-group-item'><a class='searchitem' href='#' data-id='" + data[index].number + "'>" + data[index].name + "</a></li>");
+        searchList.append("<li class='list-group-item'><a class='searchitem' href='#' data-id='" + data[index].number + "'>" + data[index].name + "</a></li>");
       }
-      $(".searchitem").on("click", function(){
-        var foodnr= $(this).attr("data-id");
+      $(".searchitem").on("click", function() {
+        var foodId = $(this).attr("data-id");
         $.ajax({
-          url: callurl + "/" + foodnr,
-          dataType : "JSON",
-        }).done(function(data){
+          url: callurl + "/" + foodId,
+          dataType: "JSON",
+        }).done(function(data) {
           console.log(data);
-          $("#food-table tbody").append("<tr><td></td><td>" + data.name +"</td></tr>");
-          $("#ulfood").empty();
-          $("#foodinput").val("");
+          $("#food-table tbody").append("<tr><td></td><td>" + data.name + "</td></tr>");
+          searchList.empty();
+          foodInput.val("");
 
-        }).fail(function(){
+        }).fail(function() {
           console.log("failfish");
         });
       });
-    }).fail(function(){
+    }).fail(function() {
       console.log("failfish1");
     });
   }
 });
 
-function addItemToUl(obj){
-  $("#ulfood").append("<li class=" + '"list-group-item"' + ">" + data.Name + data.amount +"</li>")
+function addItemToUl(obj) {
+  searchList.append("<li class=" + '"list-group-item"' + ">" + data.Name + data.amount + "</li>")
 }
