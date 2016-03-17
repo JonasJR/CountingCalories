76cb2673c692303c308ced36493bd0730d3b9e1a
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2016-03-17 15:49:30 +0100

    Beautify and fix chart and progress

--- js/diary.js
+++ js/diary.js
@@ -1,7 +1,57 @@
 $(document).ready(function() {
+
+  var energyNeeds = JSON.parse(localStorage.getItem("energyNeeds"));
+
+  if (energyNeeds == null) {
+    energyModal = $("#energyModal");
+    energyModal.modal({
+      backdrop: 'static',
+      keyboard: false
+    })
+    energyModal.modal('show');
+  }
+
+  var addEnergyModel = $("#add-energy-modal-button");
+  addEnergyModel.on("click", function() {
+
+    var array = $("#modal-form").serializeArray();
+    var gender = array[0].value;
+    var age = array[1].value;
+    var activity = array[2].value;
+    var energyNeed = 0;
+
+    if (gender == 'male') {
+      if (age == 'young' && activity == 'low') { energyNeed = 2500 }
+      if (age == 'young' && activity == 'medium') { energyNeed = 2800 }
+      if (age == 'young' && activity == 'high') { energyNeed = 3200 }
+      if (age == 'middle' && activity == 'low') { energyNeed = 2300 }
+      if (age == 'middle' && activity == 'medium') { energyNeed = 2600 }
+      if (age == 'middle' && activity == 'high') { energyNeed = 3000 }
+      if (age == 'senior' && activity == 'low') { energyNeed = 2000 }
+      if (age == 'senior' && activity == 'medium') { energyNeed = 2300 }
+      if (age == 'senior' && activity == 'high') { energyNeed = 2600 }
+    } else {
+      if (age == 'young' && activity == 'low') { energyNeed = 2000 }
+      if (age == 'young' && activity == 'medium') { energyNeed = 2300 }
+      if (age == 'young' && activity == 'high') { energyNeed = 2500 }
+      if (age == 'middle' && activity == 'low') { energyNeed = 1800 }
+      if (age == 'middle' && activity == 'medium') { energyNeed = 2100 }
+      if (age == 'middle' && activity == 'high') { energyNeed = 2400 }
+      if (age == 'senior' && activity == 'low') { energyNeed = 1700 }
+      if (age == 'senior' && activity == 'medium') { energyNeed = 1900 }
+      if (age == 'senior' && activity == 'high') { energyNeed = 2200 }
+    }
+
+    localStorage.setItem("energyNeeds", energyNeed);
+  });
+
   var calendar = JSON.parse(localStorage.getItem("calendar"));
   var badgedays = [];
-  console.log(calendar);
+
+  if (calendar == null) {
+    showMessage("Du har inga dagar sparade i kalendern.");
+    return;
+  }
 
   var eventData = createEventData(calendar);
   $("#my-calendar").zabuto_calendar({
@@ -26,6 +76,7 @@ $(document).ready(function() {
 
 function createEventData(calendar) {
   var returndata = [];
+
   calendar.forEach(function(day) {
 
     returndata.push({
@@ -43,7 +94,8 @@ function createEventData(calendar) {
 
 
 function getBodyData(day) {
-  var body = '<div id="piechart_' + day.date + '" class="piechart"></div>'
+  var body = '<div id="piechart_' + day.date + '" class="piechart"></div>';
+  body += '<label id="kcal-day-progress-label" for="kcal-day-progress"></label><div id="kcal-day-progress" class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="70"aria-valuemin="0" aria-valuemax="100" style="width:70%"><span class="sr-only"></span></div></div>';
   return body;
 }
 
@@ -55,13 +107,13 @@ function createChart(day) {
     "theme": "none",
     "dataProvider": [ {
       "Typ": "Fett",
-      "Gram": day.fat
+      "Gram": day.totalFat
     }, {
       "Typ": "Kolhydrater",
-      "Gram": day.carb
+      "Gram": day.totalCarb
     }, {
       "Typ": "Protein",
-      "Gram": day.protein
+      "Gram": day.totalProtein
     }],
     "valueField": "Gram",
     "titleField": "Typ",
@@ -74,18 +126,30 @@ function createChart(day) {
   });
 }
 
+function createProgressbar (day) {
+  console.log(day);
+  var caloriesEatenInPercent = (day.totalKcal / 2400.0) * 100;
+  console.log(JSON.parse(localStorage.getItem("calendar")));
+  console.log(caloriesEatenInPercent);
+
+  $("#kcal-day-progress").children().first().width(caloriesEatenInPercent + "%");
+  $("#kcal-day-progress-label").text("Du har ätit " + day.totalKcal + "kcal av dina dagliga " + localStorage.getItem("energyNeeds"));
+}
+
 function addDateListeners(calendar) {
-  console.log("datelistner");
 
   calendar.forEach(function(day) {
     $("#zabuto_calendar_" + day.date).on("click", function() {
       createChart(day);
+      createProgressbar(day);
     })
   });
 }
 
 
 function getdate() {
+
+  /*
   Date.prototype.yyyymmdd = function() {
     var yyyy = this.getFullYear().toString();
     var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
@@ -94,4 +158,5 @@ function getdate() {
   };
   d = new Date();
   return d.yyyymmdd();
+  */
 }
